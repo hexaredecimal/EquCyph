dbd614ec7fd012f620ae5952330fa8b0c7c82948
hexaredecimal <mfanakagama@gmail.com>
2024-10-27 23:58:40 +0200

    equcyph: Use the new icons

--- src/equcyph/EquCyph.java
+++ src/equcyph/EquCyph.java
@@ -5,8 +5,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedList;
+import javax.imageio.ImageIO;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 
@@ -28,113 +31,147 @@ public class EquCyph extends JFrame implements ActionListener {
 	public HashMap<Integer, Plane> getTabPlanes() {
 		return tabPlanes;
 	}
-	
-	
+
 	private void addMenus() {
-		JMenuBar menubar = new JMenuBar();
-		setJMenuBar(menubar);
+		try {
+			var save_icon = new ImageIcon(this.getClass().getResource("/icons/save-16.png"));
+			var save_as_icon = new ImageIcon(this.getClass().getResource("/icons/save-as-16.png"));
+			var exit_icon = new ImageIcon(this.getClass().getResource("/icons/exit-16.png"));
+			var open_icon = new ImageIcon(this.getClass().getResource("/icons/open-file-16.png"));
+			var project_icon = new ImageIcon(this.getClass().getResource("/icons/project-16.png"));
+			var clean_icon = new ImageIcon(this.getClass().getResource("/icons/clean-16.png"));
+			var formula_icon = new ImageIcon(this.getClass().getResource("/icons/formula-fx-16.png"));
+			var lambda_icon = new ImageIcon(this.getClass().getResource("/icons/lambda-16.png"));
+			var add_tab_icon = new ImageIcon(this.getClass().getResource("/icons/add-tab-16.png"));
+			var close_tab_icon = new ImageIcon(this.getClass().getResource("/icons/close-tab-16.png"));
 
-		JMenu file = new JMenu("File");
-		menubar.add(file);
+			var about_icon = new ImageIcon(this.getClass().getResource("/icons/about-16.png"));
+			var website_icon = new ImageIcon(this.getClass().getResource("/icons/website-16.png"));
+			var book_icon = new ImageIcon(this.getClass().getResource("/icons/book-16.png"));
+			var more_info_icon = new ImageIcon(this.getClass().getResource("/icons/more-info-16.png"));
+			var software_license_icon = new ImageIcon(this.getClass().getResource("/icons/software-license-16.png"));
 
-		JMenuItem newSession = new JMenuItem("New Session");
-		newSession.addActionListener(this);
+			JMenuBar menubar = new JMenuBar();
+			setJMenuBar(menubar);
 
-		JMenuItem openSession = new JMenuItem("Open Session");
-		openSession.addActionListener(this);
+			JMenu file = new JMenu("File");
+			menubar.add(file);
 
-		JMenuItem saveSession = new JMenuItem("Save Session");
-		saveSession.addActionListener(this);
+			JMenuItem newSession = new JMenuItem("New Session", project_icon);
+			newSession.addActionListener(this);
 
-		JMenuItem saveSessionAs = new JMenuItem("Save Session As");
-		saveSessionAs.addActionListener(this);
+			JMenuItem openSession = new JMenuItem("Open Session", open_icon);
+			openSession.addActionListener(this);
 
-		JMenu recent = new JMenu("Recent");
+			JMenuItem saveSession = new JMenuItem("Save Session", save_icon);
+			saveSession.addActionListener(this);
 
-		JMenuItem clearGraph = new JMenuItem("Clear Graph");
-		clearGraph.addActionListener(action -> {
-			int selectedIndex = tabs.getSelectedIndex();
-			Plane plane = tabPlanes.get(selectedIndex);
-			plane.getFunctionList().clear();
-			plane.repaint();
-			populateTree();
-		});
+			JMenuItem saveSessionAs = new JMenuItem("Save Session As", save_as_icon);
+			saveSessionAs.addActionListener(this);
 
-		JMenuItem exit = new JMenuItem("Exit");
-		exit.addActionListener(this);
+			JMenu recent = new JMenu("Recent");
 
-		file.add(newSession);
-		file.add(openSession);
-		file.addSeparator();
-		file.add(saveSession);
-		file.add(saveSessionAs);
-		file.addSeparator();
-		file.add(recent);
-		file.addSeparator();
-		file.add(clearGraph);
-		file.add(exit);
+			JMenuItem clearGraph = new JMenuItem("Clear Graph", clean_icon);
+			clearGraph.addActionListener(action -> {
+				int selectedIndex = tabs.getSelectedIndex();
+				Plane plane = tabPlanes.get(selectedIndex);
+				plane.getFunctionList().clear();
+				plane.repaint();
+				populateTree();
+			});
 
-		JMenu tools = new JMenu("Tools");
-		menubar.add(tools);
+			JMenuItem exit = new JMenuItem("Exit", exit_icon);
+			exit.addActionListener(this);
 
-		String[] graphtypes = {"Advanced", "Cubic", "Hyperbolic", "Line", "Parabolic"};
-		for (String type : graphtypes) {
-			JMenuItem graphtype = new JMenuItem(type);
-			graphtype.addActionListener(this);
-			tools.add(graphtype);
-		}
+			file.add(newSession);
+			file.add(openSession);
+			file.addSeparator();
+			file.add(saveSession);
+			file.add(saveSessionAs);
+			file.addSeparator();
+			file.add(recent);
+			file.addSeparator();
+			file.add(clearGraph);
+			file.add(exit);
 
-		JMenu layout = new JMenu("Layout");
-		menubar.add(layout);
-		
-		JMenuItem addtab = new JMenuItem("New Tab");
-		addtab.addActionListener(action -> {
-			addTab(tabs);
-		});
-		layout.add(addtab); 
-		
-		JMenuItem removeTab = new JMenuItem("Remove Tab"); 
-		removeTab.addActionListener(action -> {
-			int i = tabs.getSelectedIndex();
-			if (i != -1) {
-				tabPlanes.remove(i);
-				tabs.remove(i);
-			}
-		});
-		layout.add(removeTab);
-		
-		JMenu help = new JMenu("Help");
-		menubar.add(help);
+			JMenu tools = new JMenu("Tools");
+			menubar.add(tools);
 
-		String[] helpitems = {"Contents", "Website", "_", "License", "Contributors", "_", "About"};
-		for (String item : helpitems) {
-			if (item.equals("_")) {
-				help.addSeparator();
-				continue;
+			String[] graphtypes = {"Advanced", "Cubic", "Hyperbolic", "Line", "Parabolic"};
+			for (String type : graphtypes) {
+				JMenuItem graphtype = new JMenuItem(type, type.equals("Advanced") ? lambda_icon : formula_icon);
+				graphtype.addActionListener(this);
+				tools.add(graphtype);
 			}
 
-			JMenuItem help_item = new JMenuItem(item);
-			help_item.addActionListener(this);
-			help.add(help_item);
+			JMenu layout = new JMenu("Layout");
+			menubar.add(layout);
+
+			JMenuItem addtab = new JMenuItem("New Tab", add_tab_icon);
+			addtab.addActionListener(action -> {
+				addTab(tabs);
+			});
+			layout.add(addtab);
+
+			JMenuItem removeTab = new JMenuItem("Remove Tab", close_tab_icon);
+			removeTab.addActionListener(action -> {
+				int i = tabs.getSelectedIndex();
+				if (i != -1) {
+					tabPlanes.remove(i);
+					tabs.remove(i);
+				}
+			});
+			layout.add(removeTab);
+
+			JMenu help = new JMenu("Help");
+			menubar.add(help);
+
+			String[] helpitems = {"Contents", "Website", "_", "License", "Contributors", "_", "About"};
+			for (String item : helpitems) {
+				if (item.equals("_")) {
+					help.addSeparator();
+					continue;
+				}
+
+				JMenuItem help_item = new JMenuItem(item);
+				if (item.equals("Contents")) {
+					help_item.setIcon(book_icon);
+				} else if (item.equals("Website")) {
+					help_item.setIcon(website_icon);
+				} else if (item.equals("License")) {
+					help_item.setIcon(software_license_icon);
+				} else if (item.equals("Contributors")) {
+					help_item.setIcon(more_info_icon);
+				} else if (item.equals("About")) {
+					help_item.setIcon(about_icon);
+				}
+				help_item.addActionListener(this);
+				help.add(help_item);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 
 	private void addPopUpMenus() {
+		var formula_icon = new ImageIcon(this.getClass().getResource("/icons/formula-fx-16.png"));
+		var lambda_icon = new ImageIcon(this.getClass().getResource("/icons/lambda-16.png"));
 		int selectedIndex = tabs.getSelectedIndex();
 		Plane plane = tabPlanes.get(selectedIndex);
 		JMenuItem info = new JMenuItem("Info");
 		JMenuItem delete = new JMenuItem("Delete");
 		delete.addActionListener(action -> {
-			if (selection == null)
-				return; 
+			if (selection == null) {
+				return;
+			}
 
-			String fx_def = selection.toString(); 
+			String fx_def = selection.toString();
 			var fx_list = plane.getFunctionList();
-			for (var func: fx_list) {
+			for (var func : fx_list) {
 				if (!fx_def.equals(func.toString())) {
 					continue;
 				}
-				
+
 				fx_list.remove(func);
 				DefaultTreeModel model = (DefaultTreeModel) functions_tree.getModel();
 				DefaultMutableTreeNode top = (DefaultMutableTreeNode) model.getRoot();
@@ -142,10 +179,9 @@ public class EquCyph extends JFrame implements ActionListener {
 				plane.repaint();
 				populateTree();
 			}
-			
+
 		});
-		
-		
+
 		JMenuItem deleteAll = new JMenuItem("Delete All");
 		deleteAll.addActionListener(action -> {
 			plane.getFunctionList().clear();
@@ -155,22 +191,22 @@ public class EquCyph extends JFrame implements ActionListener {
 
 		JMenuItem props = new JMenuItem("Properties");
 		props.addActionListener(action -> {
-			if (selection == null)
-				return; 
+			if (selection == null) {
+				return;
+			}
 
-			String fx_def = selection.toString(); 
+			String fx_def = selection.toString();
 			var fx_list = plane.getFunctionList();
 			for (int index = 0; index < fx_list.size(); index++) {
 				var func = fx_list.get(index);
 				if (!fx_def.equals(func.toString())) {
 					continue;
 				}
-				
+
 				new Properties(this, plane, index, func)
 					.setVisible(true);
 			}
 		});
-
 
 		JMenu new_fx = new JMenu("new");
 
@@ -180,7 +216,7 @@ public class EquCyph extends JFrame implements ActionListener {
 				new_fx.add(new JSeparator());
 				continue;
 			}
-			JMenuItem graphtype = new JMenuItem(type);
+			JMenuItem graphtype = new JMenuItem(type, type.equals("Advanced") ? lambda_icon : formula_icon);
 			graphtype.addActionListener(this);
 			new_fx.add(graphtype);
 		}
@@ -202,7 +238,7 @@ public class EquCyph extends JFrame implements ActionListener {
 			addTab(tabs);
 		}
 		selectedIndex = tabs.getSelectedIndex();
-		
+
 		Plane plane = tabPlanes.get(selectedIndex);
 		Function f = new Function(equation, name);
 		f.setColor(style);
@@ -238,13 +274,11 @@ public class EquCyph extends JFrame implements ActionListener {
 		split.add(tabs);
 		add(split);
 
-
 		DefaultTreeModel model = (DefaultTreeModel) functions_tree.getModel();
 		DefaultMutableTreeNode top = new DefaultMutableTreeNode("functions");
 		top.removeAllChildren();
 		model.reload();
 		model.setRoot(top);
-
 
 		addMenus();
 		addPopUpMenus();
@@ -263,11 +297,10 @@ public class EquCyph extends JFrame implements ActionListener {
 		int tabIndex = tabs.getTabCount();
 		tabPlanes.put(tabIndex, plane);  // Associate the Plane with the tab index
 
-		tabs.add(String.format("Graph %d", tabIndex + 1),plane);
+		tabs.add(String.format("Graph %d", tabIndex + 1), plane);
 		tabs.setTabComponentAt(tabIndex, new ButtonTabComponent(this, tabs));
 	}
 
-	
 	public final void populateTree() {
 		DefaultTreeModel model = (DefaultTreeModel) functions_tree.getModel();
 		DefaultMutableTreeNode top = new DefaultMutableTreeNode("functions");
@@ -314,8 +347,7 @@ public class EquCyph extends JFrame implements ActionListener {
 			case "Contributors": {
 
 				String message
-					= 
-"""
+					= """
 <html>
 	<h1 align="center">Equcyph</h1>
 	<h3>Lead developer</h3>
@@ -329,8 +361,8 @@ public class EquCyph extends JFrame implements ActionListener {
 			}
 			break;
 			case "About": {
-				String art = 
-"""
+				String art
+					= """
 <br>
 <p>   Y<p>
 <p>   ^</p>
